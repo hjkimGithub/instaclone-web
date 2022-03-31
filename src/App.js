db2afda56a33261e3f6ea1167203b39d39728c99
hjkim <rlaguswo3408@gmail.com>
2022-03-31 14:01:34 +0900

    react useState POC(simple auth)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import {BrowserRouter as Router, Route, Navigate, Routes} from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 // import NotFound from "./screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div> 
       <Router>
         <Routes>
-          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="/" element={isLoggedIn ? 
+            <Home setIsLoggedIn = {setIsLoggedIn}/> : 
+            <Login setIsLoggedIn = {setIsLoggedIn}/>} 
+          />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
