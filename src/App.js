7322f49f10f7afd2039502150e6823962f014b3b
hjkim <rlaguswo3408@gmail.com>
2022-04-03 11:55:24 +0900

    styled components Theme change POC

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import { useReactiveVar } from "@apollo/client";
 // import { useState } from "react";
 import {BrowserRouter as Router, Route, Navigate, Routes} from "react-router-dom";
-import { isLoggedInVar } from "./apollo";
+import { isLoggedInVar, darkModeVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 // import NotFound from "./screens/NotFound";
 
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#2c2c2c",
+};
+
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div> 
+    <div>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}> 
       <Router>
         <Routes>
           <Route path="/" element={isLoggedIn ? <Home/> : <Login />}/>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
+      </ThemeProvider>
     </div>
   );
 }
