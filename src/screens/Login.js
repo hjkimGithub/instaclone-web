604a96e39b094b2449376dc5e5f62284de11e4f6
hjkim <rlaguswo3408@gmail.com>
2022-04-06 23:03:06 +0900

    justify styles, login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,29 +5,118 @@ const Title = styled.h1`
 `;
 
 const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const WhiteBox = styled.div`
+  background-color: white;
+  border: 1px solid rgb(219, 219, 219);
+  width: 100%;
+`;
+
+const TopBox = styled(WhiteBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+  form {
+    margin-top: 35px;
+    width: 100%;
     display: flex;
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
+    justify-items: center;
     flex-direction: column;
+    align-items: center;
+    input {
+      width: 100%;
+      border-radius: 3px;
+      padding: 7px;
+      background-color: #fafafa;
+      border: 0.5px solid rgb(219, 219, 219);
+      margin-top: 5px;
+      box-sizing: border-box;
+      &::placeholder {
+        font-size: 12px;
+      }
+      &:last-child {
+        border: none;
+        margin-top: 12px;
+        background-color: #0095f6;
+        color: white;
+        text-align: center;
+        padding: 8px 0px;
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+const BottomBox = styled(WhiteBox)`
+  padding: 20px 0px;
+  text-align: center;
+  a {
+    font-weight: 600;
+    color: #0095f6;
+  }
+`;
+
+const Wrapper = styled.div`
+  max-width: 350px;
+  width: 100%;
+`;
+
+const Separator = styled.div`
+  margin: 20px 0px 30px 0px;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  div {
+    width: 100%;
+    height: 1px;
+    background-color: rgb(219, 219, 219);
+  }
+  span {
+    margin: 0px 10px;
+    font-weight: 600;
+    color: #8e8e8e;
+  }
+`;
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+  }
 `;
 
 function Login() {
     return (
         <Container>
-            <div>
-                <h1>Instagram</h1>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <input type="submit" placeholder="Log in" />
-                </form>
-                <span>Or</span>
-                <span>Log in with Facebook</span>
-            </div>
-            <div>
-                <span>Don't have an account?</span>
-            </div>
+            <Wrapper>
+                <div>
+                    <h1>Instagram</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <input type="submit" placeholder="Log in" />
+                    </form>
+                    <span>Or</span>
+                    <FacebookLogin>
+                        <span>Log in with Facebook</span>
+                    </FacebookLogin>
+                </div>
+                <div>
+                    <span>Don't have an account?</span>
+                </div>
+            </Wrapper>
         </Container>
     );
 }
